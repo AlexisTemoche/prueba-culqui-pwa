664ff8083fcd71ba9c83dfc21093a0894c74dbd8
Alexis <alexis@temoche.com>
2023-12-14 09:26:31 -0500

    docs updated

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/prueba-culqui-pwa/sw.js', { scope: '/prueba-culqui-pwa/' })})}
